0492a45896a0b3fcf47f6668490881fb1afa1aa0
Russell Jones <justyrones@gmail.com>
2023-05-10 15:10:43 -0600

    done

--- app.js
+++ app.js
@@ -2,21 +2,71 @@
 
 const express = require("express");
 const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded());
+
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { ExpressError,
+  NotFoundError,
+  UnauthorizedError,
+  BadRequestError,
+  ForbiddenError, } = require("./expressError");
+
+  const { convertStrNums } = require("./utils");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
-
+const { findMean,
+  findMedian,
+  findMode } = require("./stats");
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res) {
+  if(!req.query.nums){
+    throw new BadRequestError("nums are required")
+  }
+  let stringNums = req.query.nums.split(",");
 
+  const convertedStrNums = convertStrNums(stringNums);
+
+  const mean = findMean(convertedStrNums);
+
+  return res.json({
+    "operation": "mean",
+    "result": mean});
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
+  if(!req.query.nums){
+    throw new BadRequestError("nums are required")
+  }
+  let stringNums = req.query.nums.split(",");
 
+  const convertedStrNums = convertStrNums(stringNums);
+
+  const median = findMedian(convertedStrNums);
+
+  return res.json({
+    "operation": "median",
+    "result": median});
+});
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res) {
+  if(!req.query.nums){
+    throw new BadRequestError("nums are required")
+  }
+  let stringNums = req.query.nums.split(",");
 
+  const convertedStrNums = convertStrNums(stringNums);
+
+  const mode = findMode(convertedStrNums);
+
+  return res.json({
+    "operation": "mode",
+    "result": mode});
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res) {
--- utils.js
+++ utils.js
@@ -6,6 +6,13 @@ const { BadRequestError } = require("./expressError");
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  for (let i=0; i<strNums.length; i++){
+    strNums[i] = (+strNums[i]);
+    if ( isNaN(strNums[i])){
+      return new BadRequestError ("must be numbers only")
+    }
+  }
+  return strNums
 }
 
 
